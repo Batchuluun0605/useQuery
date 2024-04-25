56993eb0eb91e78a0add0bbdf9a019fbab757f52
batchuluun0605 <batchuluun655@gmail.com>
2024-04-25 23:29:02 +0800

    a

---
+++ usequery-filter/app/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useCallback, useRef } from "react";
+import { mockData } from "./constant";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const filterRef = useRef("");
+
+  const filter = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const handleChange = (e: any) => {
+    filterRef.current = e.target.value;
+    router.push(pathname + "?" + filter("sort", e.target.value));
+  };
+  const filterData = mockData.filter((el) => {
+    return filterRef.current == "" ? el : el.name.includes(filterRef.current);
+  });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="flex justify-center text-[30px]">
+          sain baitsgaana uu odoo bi usequery ashiglah bolno
+        </h1>
+        <select
+          name=""
+          onChange={handleChange}
+          id="1"
+          className="p-3 border-yellow-600 border-2 rounded-xl"
+        >
+          <option value="">bugd</option>
+          <option value="burger">burger</option>
+          <option value="pizza">pizza</option>
+        </select>
+
+        {filterData.map((e, index) => {
+          return (
+            <div key={index + e.name} className="flex justify-between ">
+              <img src={e.img} alt="" />
+              <p className="text-[25px] text-orange-700">{e.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+}
